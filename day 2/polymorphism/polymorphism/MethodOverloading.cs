9c5b365871505a7e74080da223b23118d032bbbf
AkhilMukesh <44767186+AkhilMukesh@users.noreply.github.com>
2020-12-21 14:08:25 +0530

    Add files via upload

---
+++ day 2/polymorphism/polymorphism/MethodOverloading.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/// <summary>
+/// Plymorphism- Method Overloading or compile time polymorphsim.
+/// method hving same name but different parameters is knows as method overloading.
+/// </summary>
+namespace polymorphism
+{
+    class MethodOverloading
+    {
+
+        public void add(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+
+        public void add(int a, float b)
+        {
+            Console.WriteLine(a + b);
+        }
+
+        static void Main()
+        {
+            MethodOverloading p = new MethodOverloading();
+            p.add(2, 5f);
+            p.add(2, 8);
+        }
+
+    }
+}

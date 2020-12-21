b45d6cd423c4b64fa3f2eaacfba5a288ec18c124
AkhilMukesh <44767186+AkhilMukesh@users.noreply.github.com>
2020-12-21 13:59:00 +0530

    Add files via upload

---
+++ day 2/Constructor/Constructor/ExplicitConstructor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//if you create instance number of times that many times the constructor will be called.
+namespace Constructor
+{
+    class ExplicitConstructor
+    {
+        public ExplicitConstructor()
+        {
+            Console.WriteLine("constructor is called");
+        }
+
+        static void Main()
+        {
+            ExplicitConstructor ec = new ExplicitConstructor();
+
+
+        }
+    }
+}
